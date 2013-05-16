7458e8e45128a2cfdfa18ffd966f4126f2918c96
James Gray <james@james4k.com>
2013-05-16 18:00:28 -0500

    added Kcache option for makeKey

--- key.go
+++ key.go
@@ -11,11 +11,13 @@ import (
 type KeyMask int
 
 const (
+	// TODO: perhaps Kmethod should just always be implied
 	Kmethod = 1 << iota
 	Kscheme
 	Khost
 	Kpath
 	Kquery
+	Kcookie
 	Kdefault = Kmethod | Kscheme | Kpath | Kquery
 )
 
@@ -37,6 +39,13 @@ func makeKey(mask KeyMask, req *http.Request) (sum string, crc uint32) {
 	}
 	if mask&Kquery != 0 {
 		buf.WriteString(req.URL.RawQuery)
+	}
+	if mask&Kcookie != 0 {
+		if cookies, ok := req.Header["Cookie"]; ok {
+			for _, co := range cookies {
+				buf.WriteString(co)
+			}
+		}
 	}
 	h.Write(buf.Bytes())
 	sum = hex.EncodeToString(h.Sum(sumb))
